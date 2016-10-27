5a0843be7db19a454a2c3768dc28836ad4d161ce
Diggory Blake <diggsey@googlemail.com>
2016-10-27 23:28:59 +0100

    Use winit to create dx11 windows

--- src/window/dxgi/src/lib.rs
+++ src/window/dxgi/src/lib.rs
@@ -16,22 +16,20 @@
 
 #[macro_use]
 extern crate log;
-extern crate kernel32;
-extern crate user32;
 extern crate winapi;
-extern crate gfx_core;
-extern crate gfx_device_dx11;
+extern crate winit;
+extern crate gfx_core as core;
+extern crate gfx_device_dx11 as device_dx11;
 
-mod window;
-
-use std::mem;
-use gfx_core::format;
-use gfx_core::tex::Size;
-use gfx_device_dx11::{Device, Factory, Resources};
+use winit::os::windows::WindowExt;
+use core::format;
+use core::factory::Typed;
+use core::tex::Size;
+use device_dx11::{Device, Factory, Resources};
 
 
 pub struct Window {
-    hwnd: winapi::HWND,
+    inner: winit::Window,
     swap_chain: *mut winapi::IDXGISwapChain,
     driver_type: winapi::D3D_DRIVER_TYPE,
     pub size: (Size, Size),
@@ -46,19 +44,9 @@ impl Window {
         unsafe{ (*self.swap_chain).Present(wait as winapi::UINT, 0) };
     }
 
-    pub fn dispatch(&self) -> bool {unsafe {
-        let mut msg: winapi::MSG = mem::zeroed();
-        while user32::PeekMessageW(&mut msg, self.hwnd, 0, 0, winapi::PM_REMOVE) == winapi::TRUE {
-            match msg.message & 0xFFFF {
-                winapi::WM_QUIT | winapi::WM_CLOSE => return false,
-                winapi::WM_KEYDOWN if msg.wParam as i32 == winapi::VK_ESCAPE => return false,
-                _ => ()
-            }
-            user32::TranslateMessage(&msg);
-            user32::DispatchMessageW(&msg);
-        }
-        true
-    }}
+    pub fn poll_events(&self) -> winit::PollEventsIterator {
+        self.inner.poll_events()
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -73,22 +61,27 @@ pub enum InitError {
 
 /// Initialize with a given size. Typed format version.
 pub fn init<Cf>(title: &str, requested_width: u16, requested_height: u16)
-           -> Result<(Window, Device, Factory, gfx_core::handle::RenderTargetView<Resources, Cf>), InitError>
+           -> Result<(Window, Device, Factory, core::handle::RenderTargetView<Resources, Cf>), InitError>
 where Cf: format::RenderFormat
 {
-    use gfx_core::factory::Typed;
     init_raw(title, requested_width as winapi::INT, requested_height as winapi::INT, Cf::get_format())
         .map(|(window, device, factory, color)| (window, device, factory, Typed::new(color)))
 }
 
 /// Initialize with a given size. Raw format version.
 pub fn init_raw(title: &str, requested_width: winapi::INT, requested_height: winapi::INT, color_format: format::Format)
-                -> Result<(Window, Device, Factory, gfx_core::handle::RawRenderTargetView<Resources>), InitError> {
-    let hwnd = match window::create(title, requested_width, requested_height) {
-        Ok(h) => h,
-        Err(()) => return Err(InitError::Window),
+                -> Result<(Window, Device, Factory, core::handle::RawRenderTargetView<Resources>), InitError> {
+    let inner = match winit::WindowBuilder::new()
+        .with_title(title.to_owned())
+        .with_dimensions(requested_width as u32, requested_height as u32)
+        .with_min_dimensions(requested_width as u32, requested_height as u32)
+        .with_max_dimensions(requested_width as u32, requested_height as u32)
+        .build()
+    {
+        Ok(w) => w,
+        Err(_) => return Err(InitError::Window),
     };
-    let (width, height) = window::show(hwnd).unwrap();
+    let (width, height) = inner.get_inner_size_pixels().unwrap();
 
     let driver_types = [
         winapi::D3D_DRIVER_TYPE_HARDWARE,
@@ -100,7 +93,7 @@ pub fn init_raw(title: &str, requested_width: winapi::INT, requested_height: win
         BufferDesc: winapi::DXGI_MODE_DESC {
             Width: width as winapi::UINT,
             Height: height as winapi::UINT,
-            Format: match gfx_device_dx11::map_format(color_format, true) {
+            Format: match device_dx11::map_format(color_format, true) {
                 Some(fm) => fm,
                 None => return Err(InitError::Format(color_format)),
             },
@@ -117,7 +110,7 @@ pub fn init_raw(title: &str, requested_width: winapi::INT, requested_height: win
         },
         BufferUsage: winapi::DXGI_USAGE_RENDER_TARGET_OUTPUT,
         BufferCount: 1,
-        OutputWindow: hwnd,
+        OutputWindow: inner.get_hwnd() as winapi::HWND,
         Windowed: winapi::TRUE,
         SwapEffect: winapi::DXGI_SWAP_EFFECT_DISCARD,
         Flags: 0,
@@ -125,11 +118,11 @@ pub fn init_raw(title: &str, requested_width: winapi::INT, requested_height: win
 
     info!("Creating swap chain of size {}x{}", width, height);
     for dt in driver_types.iter() {
-        match gfx_device_dx11::create(*dt, &swap_desc, color_format) {
+        match device_dx11::create(*dt, &swap_desc, color_format) {
             Ok((device, factory, chain, color)) => {
                 info!("Success with driver {:?}, shader model {}", *dt, device.get_shader_model());
                 let win = Window {
-                    hwnd: hwnd,
+                    inner: inner,
                     swap_chain: chain,
                     driver_type: *dt,
                     size: (width as Size, height as Size),
